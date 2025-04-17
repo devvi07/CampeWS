b79bb838186d3d68ce601e8397c31298c67040d3
developer Calva <developercalva@gmail.com>
2025-04-16 20:50:30 -0600

    Se cambia la estructura del objeto de facturas/pagos

--- models/Pago.js
+++ models/Pago.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const pagoSchema = new mongoose.Schema({
-  factura: { type: mongoose.Schema.Types.ObjectId, ref: 'Factura' },
+  //factura: { type: mongoose.Schema.Types.ObjectId, ref: 'Factura' },
   monto: Number,
   metodo: String,
   fecha: { type: Date, default: Date.now }
