07e71a16dfc49e757ec88b131f4d68f18e468619
developer Calva <developercalva@gmail.com>
2025-04-22 18:55:27 -0600

    Optimizando el backend

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
 const connectDB = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Conexión a base de datos
 connectDB();
 
+// Middlewares
 app.use(cors());
-//app.use(express.json());
-// Aumenta el límite de tamaño para JSON
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(compression());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-const rutasTipoUsuario = require("./routes/tipoUsuario");
-const rutasUsuarios = require("./routes/usuarios");
-const rutasFacturas = require("./routes/facturas");
-const rutasPagos = require("./routes/pago");
-const rutasRutas = require("./routes/rutas");
+// Rutas
+app.use("/api/tipoUsuario", require("./routes/tipoUsuario"));
+app.use("/api/usuario", require("./routes/usuarios"));
+app.use("/api/facturas", require("./routes/facturas"));
+app.use("/api/pagos", require("./routes/pago"));
+app.use("/api/rutas", require("./routes/rutas"));
 
-app.use("/api/tipoUsuario", rutasTipoUsuario);
-app.use("/api/usuario", rutasUsuarios);
-app.use("/api/facturas", rutasFacturas);
-app.use("/api/pagos", rutasPagos);
-app.use("/api/rutas", rutasRutas);
+// Monitor RAM (solo para debug)
+setInterval(() => {
+  const used = process.memoryUsage().rss / 1024 / 1024;
+  console.log(`🧠 Memoria usada: ${used.toFixed(2)} MB`);
+}, 15000);
 
+// Inicio del servidor
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
